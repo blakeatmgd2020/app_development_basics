cbd8a2429a65c19c22009d13fd08c7e959ac8bfc
Blake Wallick <blake@mgdknockoutservices.com>
2021-12-10 10:40:06 -1000

    Completed Additional App Development pt.1

--- app_development_basics/ViewController.swift
+++ app_development_basics/ViewController.swift
@@ -5,15 +5,36 @@
 //  Created by Blake Wallick on 12/10/21.
 //
 
+
+//passControlToNextVC
+
+
 import UIKit
 
 class ViewController: UIViewController {
+
+	@IBOutlet weak var myView: UIView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
+
+	@IBAction func onButtonClick(_ sender: Any) {
+
+		UIView.animate(withDuration: 2.0) {
+
+			self.myView.backgroundColor = UIColor.green
+
+		} completion: { _ in
+			self.performSegue(withIdentifier: "passControlToNextVC", sender: self)
+		}
+
+
+		//performSegue(withIdentifier: "passControlToNextVC", sender: self)
+
+	}
 
 }
 
